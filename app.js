6e87f1c4e7413c11d353a1c080c9d02b8c370780
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-13 21:46:24 -0400

    Add submitting of articles and upvoting
    The very core of the system

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ var app = angular.module('crapperNews', []);
 app.controller('MainCtrl', [
   '$scope',
   function($scope) {
-    $scope.test = 'Hello World!';
+
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 1},
+      {title: 'post 3', upvotes: 25},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ];
+
+    $scope.addPost = function() {
+      // if($scope.title) {
+        $scope.posts.push({
+          title: $scope.title,
+          link: $scope.link,
+          upvotes: 0});
+        $scope.title='';
+        $scope.link='';
+      // }
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
+
   }
 ]);
