04346b3d6d94a5d26c4264a07b70541b3d43d29d
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-17 02:19:13 -0400

    Create REST route for upvoting comments

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
-/* PARAM for loading pre-loading post */
+/* PARAM for loading post */
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
@@ -68,6 +68,27 @@ router.post('/posts/:post/comments/', function(req, res, next) {
   });
 });
 
+/* PARAM for loading comment */
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function(err, comment) {
+    if(err) { return next(err); }
+    if(!comment) { return next(new Error("Comment not found")) };
+
+    req.comment = comment;
+
+    return next();
+  })
+})
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if(err) { return next(err); }
+
+    res.json(comment);
+  });
+});
 
 
 /* GET home page. */
