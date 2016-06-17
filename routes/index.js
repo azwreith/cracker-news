33afb546143b1b57c52d942443d8dce373f1380e
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-17 01:58:14 -0400

    REST route for posting comments

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-/* POST a post */
+/* POST post */
 router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
@@ -38,12 +38,12 @@ router.param('post', function(req, res, next, id) {
   })
 })
 
-/* GET a particular post */
+/* GET particular post */
 router.get('/posts/:post', function(req, res) {
   res.json(req.post);
 });
 
-/* UPVOTE a post */
+/* PUT upvote post */
 router.put('/posts/:post/upvote', function(req, res, next) {
   req.post.upvote(function(err, post) {
     if(err) { return next(err); }
@@ -51,6 +51,23 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     res.json(post);
   });
 });
+
+/* POST new comment */
+router.post('/posts/:post/comments/', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  comment.save(function(err, comment) {
+    if(err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err) { return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
 
 
 /* GET home page. */
