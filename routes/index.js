f83ee303c9dcc13d1a30e86d6a7ee2fc607538ce
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-17 01:46:24 -0400

    REST route for upvoting posts

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var Comment = mongoose.model('Comment');
 /* GET all Posts */
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts) {
-    if(err) { return next(err) };
+    if(err) { return next(err); }
 
     res.json(posts);
   });
@@ -19,7 +19,7 @@ router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
   post.save(function(err, post) {
-    if(err) { return next(err) };
+    if(err) { return next(err); }
 
     res.json(post);
   });
@@ -30,8 +30,8 @@ router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
   query.exec(function(err, post) {
-    if(err) { return next(err) };
-    if(!post) { return next(new Error("Can't find post")) };
+    if(err) { return next(err); }
+    if(!post) { return next(new Error("Can't find post")); }
 
     req.post = post;
     return next();
@@ -43,7 +43,14 @@ router.get('/posts/:post', function(req, res) {
   res.json(req.post);
 });
 
+/* UPVOTE a post */
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if(err) { return next(err); }
 
+    res.json(post);
+  });
+});
 
 
 /* GET home page. */
