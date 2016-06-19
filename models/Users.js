6158bfe1e9df445a4b6f7bbf7db65542ee2cdb21
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-18 23:15:59 -0400

    Add setPassword() to User model

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
 	username: {
@@ -9,5 +10,11 @@ var UserSchema = new mongoose.Schema({
 	hash: String,
 	salt: String
 });
+
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = cryto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
 
 mongoose.model('User', UserSchema);
