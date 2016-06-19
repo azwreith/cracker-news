7e831b267548cc31e18125eb725b64fed2911b2b
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-19 00:32:13 -0400

    Add passport.js

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy(
+	function(username, passport, done) {
+		User.findOne({
+			username: username
+		}, function(err, user) {
+			if (err) {
+				return done(err);
+			}
+			if (!user) {
+				return done(null, false, {
+					message: 'Incorrect username.'
+				});
+			}
+			if (!user.validPassword(password)) {
+        return done(null, false, {
+          message: 'Incorrect password. '
+        });
+			}
+      return done(null, user);
+		});
+	}
+));
