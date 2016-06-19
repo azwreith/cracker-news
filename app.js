52e112c341245ddd493e1cae1ace92ccc6dfad56
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-19 00:36:40 -0400

    Set up Passport for application

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
@@ -26,6 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
