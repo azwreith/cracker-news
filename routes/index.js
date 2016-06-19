0ea4bb2ef99823497f59ce4550ffa536a9f90e6d
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-19 01:20:30 -0400

    Add '/register' route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,29 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+/* Register the user */
+router.post('/register', function(req, res, next) {
+	if (!req.body.username || !req.body.password) {
+		return res.status(400).json({
+			message: 'Please fill all the fields'
+		});
+	}
+	var user = new User();
+
+	user.username = req.body.username;
+	user.setPassword(req.body.password);
+
+	user.save(function(err) {
+		if (err) {
+			return next(err);
+		}
+
+		return res.json({
+			token: user.generateJWT()
+		});
+	});
+});
+
 /* GET all Posts */
 router.get('/posts', function(req, res, next) {
 	Post.find(function(err, posts) {
