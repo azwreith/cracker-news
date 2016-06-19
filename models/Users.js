db06554b0fb86d958694d03066ba5dfd8fda4e24
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-18 23:45:44 -0400

    Generate JWT

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var jwt = require('jwt');
 
 var UserSchema = new mongoose.Schema({
 	username: {
@@ -12,15 +13,29 @@ var UserSchema = new mongoose.Schema({
 });
 
 UserSchema.methods.setPassword = function(password) {
-  this.salt = crypto.randomBytes(16).toString('hex');
+	this.salt = crypto.randomBytes(16).toString('hex');
 
-  this.hash = cryto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	this.hash = cryto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
 UserSchema.methods.validPassword = function(password) {
-  var hash = cryto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	var hash = cryto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 
-  return this.hash === hash;
+	return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+
+	/* set expiration to 60 days */
+	var today = new Data();
+	var exp = new Date();
+	exp.setDate(today.getDate() + 60);
+
+	return jwt.sign({
+		_id: this._id,
+		username: this.username,
+		exp: parseInt(exp.getTime() / 1000)
+	}, 'THECAKEISALIE');
 };
 
 mongoose.model('User', UserSchema);
