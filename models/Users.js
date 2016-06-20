53b16bdb3576ea290f1365deac56399414e6b961
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-20 05:58:27 -0400

    Fix derp

--- models/Users.js
+++ models/Users.js
@@ -15,11 +15,11 @@ var UserSchema = new mongoose.Schema({
 UserSchema.methods.setPassword = function(password) {
 	this.salt = crypto.randomBytes(16).toString('hex');
 
-	this.hash = cryto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
 UserSchema.methods.validPassword = function(password) {
-	var hash = cryto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 
 	return this.hash === hash;
 };
