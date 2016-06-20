187632e151cc6615dc0e215d2c18628684d7b318
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-20 05:25:24 -0400

    Add navbar

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -176,6 +176,16 @@ app.controller('AuthCtrl', [
 	}
 ]);
 
+app.controller('NavCtrl', [
+	'$scope',
+	'auth',
+	function($scope, auth) {
+		$scope.isLoggedIn = auth.isLoggedIn;
+		$scope.currentUser = auth.currentUser;
+		$scope.logOut = auth.logOut;
+	}
+]);
+
 app.controller('MainCtrl', [
 	'$scope',
 	'posts',
